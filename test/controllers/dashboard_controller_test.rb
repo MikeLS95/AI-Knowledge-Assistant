b1176f6ba6cdcdc14c9243793794d2966c490224
Mike <michaellaishleys@gmail.com>
2025-11-06 21:20:34 +1000

    fix deploy errors: 9

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -4,7 +4,7 @@ class DashboardControllerTest < ActionController::TestCase
   include Devise::Test::ControllerHelpers
 
   setup do
-    @user = users(:one)  # or create a user however you normally do
+    @user = User.create!(email: "test@example.com", password: "password123")
   end
 
   test "should get index" do
