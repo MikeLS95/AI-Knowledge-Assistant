ee2de107d0eccd94e4e6db16cabf38e1a260e25e
Mike <michaellaishleys@gmail.com>
2025-11-10 13:55:27 +1000

    fix build error

--- db/migrate/20251107052403_add_fields_to_documents.rb
+++ db/migrate/20251107052403_add_fields_to_documents.rb
@@ -1,6 +1,6 @@
 class AddFieldsToDocuments < ActiveRecord::Migration[8.1]
   def change
-    add_column :documents, :title, :string
-    add_column :documents, :content, :text
+    add_column :documents, :title, :string unless column_exists?(:documents, :title)
+    add_column :documents, :content, :text unless column_exists?(:documents, :content)
   end
 end
