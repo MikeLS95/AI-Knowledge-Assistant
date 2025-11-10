0a75ff34e81d8e76754fb9310c5ad98f9b8bb547
Mike <michaellaishleys@gmail.com>
2025-11-10 13:50:39 +1000

    fix build error

--- db/migrate/20251107044639_add_user_id_to_documents.rb
+++
@@ -1,6 +0,0 @@
-class AddUserIdToDocuments < ActiveRecord::Migration[8.1]
-  def change
-    add_reference :documents, :user, null: true, foreign_key: true
-    add_index :documents, :user_id, :created_at
-  end
-end
